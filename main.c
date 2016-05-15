634f81dd3778f2bed8cf1fcbb9d95d8f3065fc72
Nathan Sweet <nathanjsweet@gmail.com>
2016-05-14 23:07:04 -0500

    trying to create a threadsafe write function

--- main.c
+++ main.c
@@ -14,16 +14,15 @@ int main(int argc, char* argv[])
     index1 = atoi(argv[2]);
   printf("index0 is %d:%s, index1 is %d:%s\n", index0, argv[1], index1, argv[2]);
   struct pfsocket* sock0 = pfsocket(index0);
-  if((int)sock0 == -1){
-    fprintf(stderr, "ethernet, index %d, is invalied\n", index0);
+  if((int)sock0 < 0){
+    fprintf(stderr, "ethernet, index %d, is invalid: %d\n", index0, errno);
     return 1;
   }
-  /*
   struct pfsocket* sock1 = pfsocket(index1);
-  if((int)sock1 == -1){
-    fprintf(stderr, "ethernet, index %d, is invalied\n", index0);
+  if((int)sock1 < 0){
+    fprintf(stderr, "ethernet, index %d, is invalid: %d\n", index0, errno);
     return 1;
-    }*/
+  }
   int err = pf_listen(sock0, sock0_listener);
   fprintf(stderr, "error: %d\n", err);
   return 0;
@@ -56,63 +55,107 @@ struct pfsocket* pfsocket(int eth_index)
   int val = TPACKET_V1;
   setsockopt(pfsock->sock, SOL_PACKET, PACKET_VERSION, &val, sizeof(val));
   setsockopt(pfsock->sock, SOL_PACKET, PACKET_RX_RING, (void*)&req, sizeof(req));
-  //setsockopt(pfsock->sock, SOL_PACKET, PACKET_TX_RING, (void*)&req, sizeof(req));
+  setsockopt(pfsock->sock, SOL_PACKET, PACKET_TX_RING, (void*)&req, sizeof(req));
 
-  int size = req.tp_block_size * req.tp_block_nr;
+  int size = req.tp_block_size * req.tp_block_nr * 2;
   uint8_t* m = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_LOCKED | MAP_POPULATE, pfsock->sock, 0);
-  if((int)m == -1){
-    printf("errno: %d\n", errno);
-    return -1;
-  }
+  if((int)m == -1)
+    return PF_ERR_SYS;
+
   pfsock->frame_nr = req.tp_frame_nr;
-  pfsock->rx_frames = (struct frame*)malloc(sizeof(struct frame) * req.tp_frame_nr);
-  /*  pfsock->tx_frames = (struct frame*)malloc(sizeof(struct frame) * req.tp_frame_nr);
-      pfsock->last_tx_index = 0;*/
+  pfsock->rx_frames = (uint8_t*)malloc(sizeof(uint8_t*) * req.tp_frame_nr);
+  pfsock->tx_frames = (struct tx_frame*)malloc(sizeof(struct tx_frame) * req.tp_frame_nr);
+  pfsock->last_tx_index = 0;
+  pfsock->listening = 0;
   
   int i,t,fr_loc;
   for(i = 0; i < pfsock->frame_nr; i++) {
     fr_loc = i * req.tp_frame_size;
-    pfsock->rx_frames[i].offset = fr_loc;
-    pfsock->rx_frames[i].mem = m + fr_loc;
+    pfsock->rx_frames[i] = m + fr_loc;
   }
-  /*  for(t = 0; t < pfsock->frame_nr; t++, i++){
+  for(t = 0; t < pfsock->frame_nr; t++, i++){
     fr_loc = i * req.tp_frame_size;
-    pfsock->tx_frames[t].offset = fr_loc;
     pfsock->tx_frames[t].mem = m + fr_loc;
-    }*/
+    pfsock->tx_frames[t].inuse = 0;
+  }
   return pfsock;
 }
 
 int pf_listen(struct pfsocket* sock, void (*fx)(uint8_t*,unsigned int))
 {
-  struct pollfd pfd;
+  if(!__sync_bool_compare_and_swap(&sock->listening, 0, 1))
+    return PF_ERR_LISTENING;
 
+  struct pollfd pfd;
   memset(&pfd, 0, sizeof(pfd));
   pfd.fd = sock->sock;
   pfd.events = POLLIN | POLLERR;
   pfd.revents = 0;
-  unsigned int frame_num = 0;
+  unsigned int rx_index = 0;
   union frame_map ppd;
-  uint8_t* pay;
   while(1) {
-    while(user_ready(sock->rx_frames[frame_num].mem)){
-      ppd.raw = sock->rx_frames[frame_num].mem;
-      pay = (uint8_t *)(ppd.raw + ppd.hdr->tp_h.tp_mac);
-      print_mac_dest(pay);
-      print_mac_source(pay);
+    for(ppd.raw = sock->rx_frames[rx_index];
+	user_ready(ppd.raw);
+	rx_index = (rx_index + 1) % sock->frame_nr) {
+
+      fx((uint8_t *)(ppd.raw + ppd.hdr->tp_h.tp_mac), ppd.hdr->tp_h.tp_len);
       set_kernel_ready(ppd.raw);
-      frame_num = (frame_num + 1) % sock->frame_nr;
     }
     if(poll(&pfd, 1, 1) == -1) {
-      return errno;
+      sock->listening = 0;
+      return PF_ERR_SYS;
     }
   }
-  return 0;
+  sock->listening = 0;
 }
 
-int pf_write(struct pfsocket* sock, uint8_t* buf, int start, int stop)
+int pf_write(struct pfsocket* sock, uint8_t* buf, int len)
 {
-  
+  struct pollfd pfd;
+  memset(&pfd, 0, sizeof(pfd));
+  pfd.fd = sock->sock;
+  pfd.events = POLLOUT | POLLERR;
+  pfd.revents = 0;
+  int tx_index = 0;
+  union frame_map ppd;
+  int tries;
+  for(tries = 0; tries < sock->frame_nr; tries++) {
+    tx_index = _get_available_tx_index(sock);
+    ppd.raw = sock->tx_frames[tx_index].mem;
+    if(!kernel_ready(ppd.raw))
+      if(poll(&pfd, 1, 1) == -1) {
+	return PF_ERR_SYS;
+      } else if(kernel_ready(ppd.raw)) {
+	goto
+      }
+  write:
+      ppd.hdr->tp_h.tp_snaplen = len;
+      ppd.hdr->tp_h.tp_len = len;
+      memcpy((uint8_t *) ppd.raw + TPACKET_HDRLEN - sizeof(struct sockaddr_ll), buf, len);
+      set_user_ready(ppd.raw);
+      sock->tx_frames[tx_index].inuse = 0;
+      return len;
+    }
+  }
+  return PF_ERR_MAX_WRITE;
+}
+
+int _get_available_frame_nr(struct pfsocket* sock)
+{
+  int tx_index;
+  for(tx_index = _iterate_tx_index(sock);
+      !__sync_bool_compare_and_swap(&sock->tx_frames[tx_index].inuse, 0, 1);
+      tx_index = _iterate_tx_index(sock));
+  return tx_index;
+}
+
+int _iterate_tx_index(struct pfsocket* sock)
+{
+  int tx_index = sock->last_tx_index;
+  while(!__sync_bool_compare_and_swap(&sock->last_tx_index,
+				      tx_index,
+				      (tx_index = (tx_index + 1) % sock->frame_nr)));
+  return tx_index;  
 }
 
 int user_ready(struct tpacket_hdr* hdr)
@@ -120,13 +163,20 @@ int user_ready(struct tpacket_hdr* hdr)
   return ((hdr->tp_status & TP_STATUS_USER) == TP_STATUS_USER);
 }
 
-int set_kernel_ready(struct tpacket_hdr* hdr)
+void set_user_ready(struct tpacket_hdr* hdr)
+{
+  hdr->tp_status = TP_STATUS_SEND_REQUEST;
+  __sync_synchronize();
+}
+
+int kernel_ready(struct tpacket_hdr* hdr)
+{
+  return !(hdr->tp_status & (TP_STATUS_SEND_REQUEST | TP_STATUS_SENDING));
+}
+
+void set_kernel_ready(struct tpacket_hdr* hdr)
 {
   hdr->tp_status = TP_STATUS_KERNEL;
-  // this is to make sure that is set_kernel_ready is inlined
-  // which it is likely to be that no memory operation is
-  // performed after the above status set. If an interrupt
-  // we're to happen that would be inconvenient.
   __sync_synchronize();
 }
 
--- main.h
+++ main.h
@@ -42,8 +42,11 @@
 # define __align_tpacket(x) __attribute__((aligned(TPACKET_ALIGN(x))))
 #endif
 
-#define NUM_PACKETS 100
 #define ALIGN_8(x) (((x) + 8 - 1) & ~(8 - 1))
+
+#define PF_ERR_SYS		-1 /* system call error; check errno */
+#define PF_ERR_LISTENING	-2 /* socket already being listened to */
+#define PF_ERR_MAX_WRITE	-3 /* socket reached maximum amount of tries to write to socket; write failure */
 
 union frame_map {
   struct {
@@ -53,24 +56,29 @@ union frame_map {
   void *raw;
 };
 
-struct frame {
-  int offset;
+struct tx_frame {
   uint8_t *mem;
+  uint8_t inuse;
 };
 
 struct pfsocket {
+  int listening;
   int sock;
   int frame_nr;
   int last_tx_index;
-  struct frame *rx_frames;
-  struct frame *tx_frames;
+  uint8_t **rx_frames;
+  struct tx_frame *tx_frames;
 };
 
 struct pfsocket* pfsocket();
 int pf_listen(struct pfsocket*, void (*fx)(uint8_t*,unsigned int));
-int pf_write(struct pfsocket*, uint8_t*, int, int);
+int pf_write(struct pfsocket*, uint8_t*, int);
+void set_user_ready(struct tpacket_hdr*);
 int user_ready(struct tpacket_hdr*);
-int set_kernel_ready(struct tpacket_hdr*);
+void set_kernel_ready(struct tpacket_hdr*);
+int kernel_ready(struct tpacket_hdr*);
+int _get_available_tx_index(struct pfsocket*);
+int _iterate_tx_index(struct pfsocket*);
 
 void print_mac_dest(uint8_t*);
 void print_mac_source(uint8_t*);
